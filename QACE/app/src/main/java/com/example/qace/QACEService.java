7d3476b0257fcefcbcdd21a1370d1cb68780db29
jaiverma <jai2.verma@outlook.com>
2019-09-11 13:34:47 +0530

    Adding notification support

--- QACE/app/src/main/java/com/example/qace/QACEService.java
+++ QACE/app/src/main/java/com/example/qace/QACEService.java
@@ -1,10 +1,13 @@
 package com.example.qace;
 
 import android.app.Notification;
+import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Handler;
 import android.os.IBinder;
 import android.os.Message;
@@ -12,6 +15,8 @@ import android.os.Messenger;
 import android.os.RemoteException;
 import android.util.Log;
 import android.widget.Toast;
+
+import androidx.core.app.NotificationCompat;
 
 import java.util.Random;
 
@@ -28,7 +33,7 @@ public class QACEService extends Service {
 
     @Override
     public void onCreate() {
-//        mNM = (NotificationManager)getSystemService(NOTIFICATION_SERVICE);
+        mNM = (NotificationManager)getSystemService(NOTIFICATION_SERVICE);
         showNotification("onCreate");
         model = new Model(this, Model.MODEL.PONG);
         randGen = new Random();
@@ -36,7 +41,7 @@ public class QACEService extends Service {
 
     @Override
     public void onDestroy() {
-//        mNM.cancel(mNotificationId);
+        mNM.cancel(mNotificationId);
         Log.i("hathi", "onDestroy");
         Toast.makeText(this, "onDestroy", Toast.LENGTH_SHORT).show();
     }
@@ -50,18 +55,27 @@ public class QACEService extends Service {
 
     private void showNotification(CharSequence title) {
         Log.i("hathi", title.toString());
-//        // In this sample, we'll use the same text for the ticker and the expanded notification
-//        CharSequence text = "QACE";
-//
-//        // Set the info for the views that show in the notification panel.
-//        Notification notification = new Notification.Builder(this)
-//                .setTicker(text)  // the status text
-//                .setWhen(System.currentTimeMillis())  // the time stamp
-//                .setContentTitle(title)  // the label of the entry
-//                .setContentText(text)  // the contents of the entry
-//                .build();
-//        // Send the notification.
-//        mNM.notify(mNotificationId, notification);
+        // In this sample, we'll use the same text for the ticker and the expanded notification
+        CharSequence text = "QACE";
+
+        NotificationChannel nChannel = new NotificationChannel("QACE", "QACE_CHANNEL", NotificationManager.IMPORTANCE_HIGH);
+        nChannel.setDescription("Q-ACE Service");
+        nChannel.enableLights(true);
+        nChannel.setLightColor(Color.RED);
+        nChannel.enableVibration(true);
+        nChannel.setShowBadge(true);
+        mNM.createNotificationChannel(nChannel);
+
+        // Set the info for the views that show in the notification panel.
+        Notification notification = new NotificationCompat.Builder(this, "QACE")
+                .setSmallIcon(R.drawable.ic_launcher_foreground)
+                .setTicker(text)  // the status text
+                .setWhen(System.currentTimeMillis())  // the time stamp
+                .setContentTitle(title)  // the label of the entry
+                .setContentText(text)  // the contents of the entry
+                .build();
+        // Send the notification.
+        mNM.notify(mNotificationId, notification);
     }
 
     class IncomingHandler extends Handler {
